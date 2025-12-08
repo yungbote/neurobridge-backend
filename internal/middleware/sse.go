bfdb7a9197588ed3cdb3f7fef44679c7af85c837
yungbote <yungbote@gmail.com>
2025-12-07 22:14:13 -0500

    fixed

--- internal/middleware/sse.go
+++ internal/middleware/sse.go
@@ -3,14 +3,12 @@ package middleware
 import (
   "github.com/gin-gonic/gin"
   "github.com/yungbote/neurobridge-backend/internal/ssedata"
-  "github.com/yungbote/neurobridge-backend/internal/errordata"
 )
 
 func AttachRequestContext() gin.HandlerFunc {
   return func(c *gin.Context) {
     ctx := c.Request.Context()
     ctx = ssedata.WithSSEData(ctx)
-    ctx = errordata.WithErrorData(ctx)
     c.Request = c.Request.WithContext(ctx)
     c.Next()
   }
